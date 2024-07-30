31f7aabe170d78a4ebcde4547d2ecd976509faa5
Zach Phillips <phillizr@udel.edu>
2024-07-30 19:39:33 -0400

    Created decrypt function. Allowed user to actually choose to encode or decode.

--- day_8/ceasar.py
+++ day_8/ceasar.py
@@ -21,5 +21,28 @@ def encrypt(text, shift):
     print(f"The encoded text is {encrypted_message}")
 
 
-#Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(text, shift):
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+    encrypted_list = []
+    decrypted_message = ""
+
+    for char in text:
+        encrypted_list.append(char)
+
+    for char in encrypted_list:
+        new_char_index = alphabet.index(char) + 26 - shift
+        decrypted_message += alphabet[new_char_index]
+
+    print(f"The decoded text is {decrypted_message}.")
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == 'encode':
+    encrypt(text, shift)
+elif direction == 'decode':
+    decrypt(text, shift)
+else:
+    print("Please choose 'encode' or 'decode'.")
