879ce70a3e4d23381bda1e93c91b648e83ae507d
Zach Phillips <phillizr@udel.edu>
2024-07-30 14:43:03 -0400

    Jumped ahead and added attempts tracking, broken though.

--- day_7/hangman.py
+++ day_7/hangman.py
@@ -1,6 +1,7 @@
 ## Making a hangman game
 
 import random
+attempts = 6
 
 # First challenge is to make a list of random words, make blanks equal to the number of letters in the word, and have user guess.
 
@@ -15,9 +16,9 @@ def chose_word():
 
 # Changes the letters into '_' to allow for guesses.
 def word_to_blanks(word):
-    blanks = ''
+    blanks = []
     for char in word:
-        blanks += '_ '
+        blanks.append("_")
     return blanks
         
 # Checks to see if the letter is in the hangman word.
@@ -25,9 +26,19 @@ def check_guess(letter):
     if letter in guesses:
         print(f"You have already guessed the letter {letter}. Please try again.")
     else:
-        for char in hangman_word:
-            if char == letter:
-                print(f"{letter} is correct.")
+        if letter in hangman_word:
+            
+            # Initiates an index to go through the list.
+            index = 0
+            for char in hangman_word:
+                # If the letter equals the char in the hangman_word, it changes the blank to it, then increments the index.
+                if char == letter:
+                    hangman_blanks[index] = letter
+                index += 1
+        else: 
+            attempts -= 1        
+                
+
     # Addes guessed letter to the guessed letters.    
     guesses.add(letter)
 
@@ -38,12 +49,14 @@ hangman_word = chose_word()
 hangman_blanks = word_to_blanks(hangman_word)
 guesses = set()
 
-while game_state:
+
+while attempts > 0:
     
+    print(f"You have {attempts} attempts left.")
     print(hangman_word)
     print(hangman_blanks)
     print(f"You have already guessed: {guesses}")
     user_guess = input("Please guess a letter: ").lower()
     check_guess(user_guess)
-
+    
 
