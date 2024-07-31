bd40ee07bf970896564787777cb7ce0758f82cd3
Zach Phillips <phillizr@udel.edu>
2024-07-31 12:15:30 -0400

    Refactored by creating calculator function. Allowed for creating new calculations. Added ability to quit out of program when done with calculator.

--- day_10/main.py
+++ day_10/main.py
@@ -1,5 +1,6 @@
 from art import logo
 
+
 def add(n1, n2):
     """Adds two numbers together
 
@@ -11,6 +12,7 @@ def add(n1, n2):
         int or float: The sum of n1 and n2
     """
     return n1 + n2
+
 
 def subtract(n1, n2):
     """Subtracts n2 from n1
@@ -24,6 +26,7 @@ def subtract(n1, n2):
     """
     return n1 - n2
 
+
 def multiply(n1, n2):
     """Produces the product of n1 and n2
 
@@ -35,6 +38,7 @@ def multiply(n1, n2):
         int or float: Product of n1 and n2
     """
     return n1 * n2
+
 
 def divide(n1, n2):
     """Division of n1 by n2
@@ -50,41 +54,43 @@ def divide(n1, n2):
         return n1 / n2
     else:
         return "Cannot divide by zero."
-    
 
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
-}
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+
+def calculator():
+    should_continue = True
+    num1 = int(input("What is the first number? "))
+
+    # Prints out the operation list
+    for key in operations.keys():
+        print(key)
+
+    while should_continue:
+
+        operation_symbol = input("Pick an operation: ")
+
+        num2 = int(input("What is the second number? "))
+
+        # Calculates the answer by finding the value from the operation_symbol function to call the appropriate function
+        answer = operations[operation_symbol](num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}.")
+
+        question = input(
+            f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation or type 'q' to quit: "
+        )
+        if question == "y":
+            num1 = answer
+        elif question == "n":
+            should_continue = False
+            calculator()
+        else:
+            exit()
+
 
 print(logo)
 
-should_continue = True
 
-num1 = int(input("What is the first number? "))
-
-# Prints out the operation list
-for key in operations.keys():
-        print(key)
-
-
-
-while should_continue:
-
-    operation_symbol = input("Pick an operation: ")
-
-    num2 = int(input("What is the second number? "))
-
-    # Calculates the answer by finding the value from the operation_symbol function to call the appropriate function
-    answer = operations[operation_symbol](num1, num2)
-
-    print(f"{num1} {operation_symbol} {num2} = {answer}.")
-    num1 = answer
-    
-
-    question = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
-    if question != 'y':
-        should_continue = False
-    
+calculator()
