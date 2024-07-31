6c57c94780fa7cbe997211a7d3b9985131fca798
Zach Phillips <phillizr@udel.edu>
2024-07-31 12:18:55 -0400

    Converted ints to floats to allow for better maths. Changed docstrings to reflect change.

--- day_10/main.py
+++ day_10/main.py
@@ -5,11 +5,11 @@ def add(n1, n2):
     """Adds two numbers together
 
     Args:
-        n1 (int or float): First number to be added
-        n2 (int or float): Second number to be added
+        n1 (float): First number to be added
+        n2 (float): Second number to be added
 
     Returns:
-        int or float: The sum of n1 and n2
+        float: The sum of n1 and n2
     """
     return n1 + n2
 
@@ -18,11 +18,11 @@ def subtract(n1, n2):
     """Subtracts n2 from n1
 
     Args:
-        n1 (int or float): The number to be subtracted from
-        n2 (int or float): The number being subtracted
+        n1 (float): The number to be subtracted from
+        n2 (float): The number being subtracted
 
     Returns:
-        int or float: The difference between n1 and n2
+        float: The difference between n1 and n2
     """
     return n1 - n2
 
@@ -31,11 +31,11 @@ def multiply(n1, n2):
     """Produces the product of n1 and n2
 
     Args:
-        n1 (int or float): First number being multiplied
-        n2 (int or float): Second number being multiplied
+        n1 (float): First number being multiplied
+        n2 (float): Second number being multiplied
 
     Returns:
-        int or float: Product of n1 and n2
+        float: Product of n1 and n2
     """
     return n1 * n2
 
@@ -44,8 +44,8 @@ def divide(n1, n2):
     """Division of n1 by n2
 
     Args:
-        n1 (int or float): Number being divided
-        n2 (int or float): Number doing division
+        n1 (float): Number being divided
+        n2 (float): Number doing division
 
     Returns:
         float: Quotient of n1 and n2
@@ -61,7 +61,7 @@ operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
 def calculator():
     should_continue = True
-    num1 = int(input("What is the first number? "))
+    num1 = float(input("What is the first number? "))
 
     # Prints out the operation list
     for key in operations.keys():
@@ -71,7 +71,7 @@ def calculator():
 
         operation_symbol = input("Pick an operation: ")
 
-        num2 = int(input("What is the second number? "))
+        num2 = float(input("What is the second number? "))
 
         # Calculates the answer by finding the value from the operation_symbol function to call the appropriate function
         answer = operations[operation_symbol](num1, num2)
