a7b9b8c2efa4996c76f2aa28e7f6769692e27d08
Zach Phillips <phillizr@udel.edu>
2024-07-30 21:46:39 -0400

    Created art file for logo. Created add_bid(). Created main loop for bidding.

--- day_9/main.py
+++ day_9/main.py
@@ -1,5 +1,28 @@
+from art import logo
 
-
+bidding = {}
 
 def clear():  # Prints 50 blank lines
     print("\n" * 50)
+
+def add_bid(name, bid):
+    bidding[name] = bid
+
+is_open = True
+
+while is_open:
+
+
+    print("Welcome to the secret auction program.")
+    name = input("What is your name: ")
+    bid = int(input("What is your bid: $"))
+
+    add_bid(name, bid)
+
+    more_bidders = input("Are there any other bidders? y/n\n")
+    if more_bidders == 'y':
+        clear()
+    else:
+        is_open = False
+
+print(bidding)
