73c0f2560124e3346bf8abe0e263148baa332f57
Zachary Phillips <phillizr@udel.edu>
2024-08-02 14:47:24 -0400

    Added make_drink function. Updated operations logic to include make_drink.

--- day_15/main.py
+++ day_15/main.py
@@ -21,7 +21,7 @@ MENU = {
             "coffee": 24,
         },
         "cost": 3.0,
-    }
+    },
 }
 
 resources = {
@@ -32,37 +32,27 @@ resources = {
 
 bank = 5.00
 
+
 def report():
     for key, value in resources.items():
         print(f"{key}: {value}")
     print(f"Cash: {bank}")
 
-# def check_espresso():
-#     if check_resources("espresso"):
-#         print("Made espresso")
-#     else:
-#         print("Could not make espresso, need more of an item.")
-
-# def check_latte():
-#     if check_resources("latte"):
-#         print("Made latte")
-#     else:
-#         print("Could not make latte, need more of an item.")
-
-# def check_espresso():
-#     if check_resources("cappuccino"):
-#         print("Made cappuccino")
-#     else:
-#         print("Could not make cappuccino, need more of an item.")
 
 def check_resources(drink: str) -> bool:
     for ingredient, amount in MENU[drink]["ingredients"].items():
         if resources[ingredient] < amount:
+            print(
+                f"Not enough {ingredient} to make {drink}. Please notify the coffee admin."
+            )
             return False
     print(f"Able to make {drink}.")
     return True
 
 
+def make_drink(drink: str):
+    for ingredient, amount in MENU[drink]["ingredients"].items():
+        resources[ingredient] -= amount
 
 
 def operations():
@@ -77,11 +67,15 @@ def operations():
             report()
         elif command == "espresso":
             check_resources(command)
+            make_drink(command)
         elif command == "latte":
             check_resources(command)
+            make_drink(command)
         elif command == "cappuccino":
             check_resources(command)
+            make_drink(command)
         else:
             print("Please enter a valid command.")
+
 
 operations()
