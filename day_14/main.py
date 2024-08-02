31cd98a825d3dfd205026021b5ade08cb364cf91
Zachary Phillips <phillizr@udel.edu>
2024-08-01 23:22:34 -0400

    Add game loop. Add round tracker. Add player score tracking.

--- day_14/main.py
+++ day_14/main.py
@@ -26,26 +26,40 @@ Game ends first time player is wrong
 
 """
 
+
 print(art.logo)
 
-random_pick_a = choice(data)
-random_pick_b = choice(data)
-pick_a_followers = random_pick_a['follower_count']
-pick_b_followers = random_pick_b['follower_count']
-higher_follower_count = ''
+# Initiates the player score.
+player_score = 0
+round = 1
 
-if pick_a_followers > pick_b_followers:
-    higher_follower_count = 'a'
-else:
-    higher_follower_count = 'b'
 
-print(f"Compare A: {random_pick_a['name']}, a {random_pick_a['description']}, from {random_pick_a['country']}")
-print(art.vs)
-print(f"Against B: {random_pick_b['name']}, a {random_pick_b['description']}, from {random_pick_b['country']}")
-answer = input("Who has more followers? Type 'a' or 'b': ")
+not_losing = True
 
-if answer == higher_follower_count:
-    print("You win")
-else:
-    print("You lose")
+while not_losing:
+
+    # Picks random choices from the game_data file.
+    random_pick_a = choice(data)
+    random_pick_b = choice(data)
+    # Finds the choice with the higher follower count. Then assigns that to either 'a' or 'b' to use when answering.
+    pick_a_followers = random_pick_a['follower_count']
+    pick_b_followers = random_pick_b['follower_count']
+    higher_follower_count = ''
+    if pick_a_followers > pick_b_followers:
+        higher_follower_count = 'a'
+    else:
+        higher_follower_count = 'b'
+    print(f"The current round is: {round}")
+    print(f"Compare A: {random_pick_a['name']}, a {random_pick_a['description']}, from {random_pick_a['country']}")
+    print(art.vs)
+    print(f"Against B: {random_pick_b['name']}, a {random_pick_b['description']}, from {random_pick_b['country']}")
+    answer = input("Who has more followers? Type 'a' or 'b': ")
+
+    if answer == higher_follower_count:
+        player_score += 1
+        round += 1
+        print("That's correct. Next round!")
+    else:
+        print(f"You lose with a final score of {player_score}")
+        not_losing = False
 
