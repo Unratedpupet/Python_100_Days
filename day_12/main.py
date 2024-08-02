f5f4e174b7e1277aff22f84bc132f5abb15467c7
Zachary Phillips <phillizr@udel.edu>
2024-08-01 22:18:30 -0400

    Fix format with Black. Not doing error or input checks.

--- day_12/main.py
+++ day_12/main.py
@@ -1,19 +1,20 @@
-#Number Guessing Game Objectives:
+# Number Guessing Game Objectives:
 
 # Include an ASCII art logo.
 # Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
 # If they got the answer correct, show the actual answer to the player.
 # Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
+# If they run out of turns, provide feedback to the player.
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
 from random import choice
 from art import logo
 
 
-EASY_LEVEL = 10 
+EASY_LEVEL = 10
 HARD_LEVEL = 5
+
 
 def choose_number() -> int:
     return choice(range(0, 101))
@@ -23,20 +24,15 @@ def choose_number() -> int:
 #     return lives - 1
 
 
-
-
-
-
 def play():
-
     print(logo)
 
     choose_difficulty = input("Choose a level. Easy 'e' or Hard 'h': ")
-    secret_number = (choose_number())
+    secret_number = choose_number()
 
-    if choose_difficulty == 'e':
+    if choose_difficulty == "e":
         lives = EASY_LEVEL
-    elif choose_difficulty == 'h':
+    elif choose_difficulty == "h":
         lives = HARD_LEVEL
     else:
         print("Please enter 'e' or 'h'.")
@@ -56,12 +52,13 @@ def play():
         else:
             print(f"You got it! The secret number was {secret_number}")
             still_guessing = False
-        
+
     print(f"You've run out of lives, the secret number was {secret_number}.")
+
 
 is_playing = True
 while is_playing:
     play()
-    if input("Play again? y/n: ") != 'y':
+    if input("Play again? y/n: ") != "y":
         print("Thank you for playing!")
         is_playing = False
