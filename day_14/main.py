98a4df76175f1927a9cba7e442cc7c9a9d601f9b
Zachary Phillips <phillizr@udel.edu>
2024-08-01 23:14:39 -0400

    Add more user friendly experience. Add logic for winning. Add comperator for higher follower count.

--- day_14/main.py
+++ day_14/main.py
@@ -28,10 +28,24 @@ Game ends first time player is wrong
 
 print(art.logo)
 
-random_pick_1 = choice(data)
-random_pick_2 = choice(data)
+random_pick_a = choice(data)
+random_pick_b = choice(data)
+pick_a_followers = random_pick_a['follower_count']
+pick_b_followers = random_pick_b['follower_count']
+higher_follower_count = ''
 
-print(f"{random_pick_1['name']}, a {random_pick_1['description']}, from {random_pick_1['country']}")
+if pick_a_followers > pick_b_followers:
+    higher_follower_count = 'a'
+else:
+    higher_follower_count = 'b'
+
+print(f"Compare A: {random_pick_a['name']}, a {random_pick_a['description']}, from {random_pick_a['country']}")
 print(art.vs)
-print(f"{random_pick_1['name']}, a {random_pick_1['description']}, from {random_pick_1['country']}")
+print(f"Against B: {random_pick_b['name']}, a {random_pick_b['description']}, from {random_pick_b['country']}")
+answer = input("Who has more followers? Type 'a' or 'b': ")
+
+if answer == higher_follower_count:
+    print("You win")
+else:
+    print("You lose")
 
