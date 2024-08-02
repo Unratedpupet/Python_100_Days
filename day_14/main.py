2fcbd8f6010885aa39e7bbe39504dd2e1089a8d9
Zachary Phillips <phillizr@udel.edu>
2024-08-01 23:01:11 -0400

    Add art file. Add game_data file. Add main file. Add basic requirements and flow.

---
+++ day_14/main.py
@@ -0,0 +1,28 @@
+import art
+from game_data import game_data
+from random import choices
+
+"""
+
+Higher vs Lower game.
+
+First print out the logo
+print out the first item from game_data
+print the vs art
+print the second item from game_data
+
+ask the use who has more followers
+
+game_data items are a list of dictionaries with the following keys:
+    name
+    follower_count
+    description
+    country
+
+keep score of how many gotten right.
+Game ends first time player is wrong
+    display message and final score.
+
+
+"""
+
