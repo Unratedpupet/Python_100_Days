870c69156f5715c457da518a701ff34ba9a070ab
Zach Phillips <phillizr@udel.edu>
2024-08-05 10:04:27 -0400

    Created QuizBrain class. Created next_question in QuizBrain. Add functionality to main to start the quiz.

---
+++ day_17/quiz_brain.py
@@ -0,0 +1,17 @@
+#TODO: Ask the questions
+#TODO: Check if answer is correct
+#TODO: Check if at end of quiz
+from question_model import Question
+
+
+class QuizBrain:
+
+    def __init__(self, question_list: list[Question]) -> None:
+        self.question_number = 0
+        self.question_list = question_list
+
+    
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        answer = input(f"Q{self.question_number}. {current_question.text}? True/False: ")
