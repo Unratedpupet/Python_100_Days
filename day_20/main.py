a6c4f06fd530dde8e245fee2ec32d89971f77223
Zach Phillips <phillizr@udel.edu>
2024-08-05 21:05:08 -0400

    Fixed the only two segment instead of three bug. Fixed movement methods to prevent backing into itself.

--- day_20/main.py
+++ day_20/main.py
@@ -11,51 +11,17 @@ screen.listen()
 
 snake = Snake()
 
-# x_increment = 0
-# for _ in range(3):
-
-#     new_snake = t.Turtle(shape="square")
-#     new_snake.color("white")
-#     new_snake.pu()
-#     new_snake.setposition(x=x_increment, y=0)
-#     snake_body.append(new_snake)
-#     x_increment -= 20
-
-
-# def move_up():
-#     snake_body[0].setheading(90)
-    
-
-# def move_down():
-#     snake_body[0].setheading(270)
-    
-
-# def move_right():
-#     snake_body[0].setheading(0)
-    
-
-# def move_left():
-#     snake_body[0].setheading(180)
-    
-
-# screen.onkey(fun=move_up, key='w')
-# screen.onkey(fun=move_down, key='s')
-# screen.onkey(fun=move_right, key='d')
-# screen.onkey(fun=move_left, key='a')
-
-
 is_alive = True
+
+# Bind the key events outside the main loop
+screen.onkey(fun=snake.move_up, key='w')
+screen.onkey(fun=snake.move_down, key='s')
+screen.onkey(fun=snake.move_right, key='d')
+screen.onkey(fun=snake.move_left, key='a')
 
 while is_alive:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
         
-        
-
-
-
-
-
 screen.exitonclick()
--- day_20/snake.py
+++ day_20/snake.py
@@ -1,38 +1,41 @@
 import turtle as t
+
 class Snake:
 
     def __init__(self) -> None:
-        
         self.snake_body = []
+        self.create_snake()
 
+    def create_snake(self):
         x_increment = 0
         for _ in range(3):
-
             new_snake = t.Turtle(shape="square")
             new_snake.color("white")
-            new_snake.pu()
+            new_snake.penup()  # Use 'penup' instead of 'pu' for clarity
             new_snake.setposition(x=x_increment, y=0)
             self.snake_body.append(new_snake)
             x_increment -= 20
 
-    # def move_up(self):
-    #     self.snake_body[0].setheading(90)
-        
+    def move_up(self):
+        if self.snake_body[0].heading() != 270:  # Prevent moving directly backward
+            self.snake_body[0].setheading(90)
 
-    # def move_down(self):
-    #     self.snake_body[0].setheading(270)
-        
+    def move_down(self):
+        if self.snake_body[0].heading() != 90:  # Prevent moving directly backward
+            self.snake_body[0].setheading(270)
 
-    # def move_right(self):
-    #     self.snake_body[0].setheading(0)
-        
+    def move_right(self):
+        if self.snake_body[0].heading() != 180:  # Prevent moving directly backward
+            self.snake_body[0].setheading(0)
 
-    # def move_left(self):
-    #     self.snake_body[0].setheading(180)
+    def move_left(self):
+        if self.snake_body[0].heading() != 0:  # Prevent moving directly backward
+            self.snake_body[0].setheading(180)
 
     def move(self):
         for snake_body_index in range(len(self.snake_body)-1, 0, -1):
-            self.snake_body[snake_body_index].setposition(self.snake_body[snake_body_index-1].pos())
+            new_x = self.snake_body[snake_body_index-1].xcor()
+            new_y = self.snake_body[snake_body_index-1].ycor()
+            self.snake_body[snake_body_index].goto(new_x, new_y)
         
-        self.snake_body[0].forward(10)
-    
+        self.snake_body[0].forward(20)
