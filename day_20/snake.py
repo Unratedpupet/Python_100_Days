c7ff16c7cbaa810b2bd9b4b687d16e086fca91de
Zach Phillips <phillizr@udel.edu>
2024-08-05 21:14:23 -0400

    Fixed turn methods. Created head segment for easier movement.

--- day_20/snake.py
+++ day_20/snake.py
@@ -1,10 +1,13 @@
 import turtle as t
 
+MOVE_DISTANCE = 20
+
 class Snake:
 
     def __init__(self) -> None:
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         x_increment = 0
@@ -17,20 +20,20 @@ class Snake:
             x_increment -= 20
 
     def move_up(self):
-        if self.snake_body[0].heading() != 270:  # Prevent moving directly backward
-            self.snake_body[0].setheading(90)
+        if self.head.heading() != 270:  # Prevent moving directly backward
+            self.head.setheading(90)
 
     def move_down(self):
-        if self.snake_body[0].heading() != 90:  # Prevent moving directly backward
-            self.snake_body[0].setheading(270)
+        if self.head.heading() != 90:  # Prevent moving directly backward
+            self.head.setheading(270)
 
     def move_right(self):
-        if self.snake_body[0].heading() != 180:  # Prevent moving directly backward
-            self.snake_body[0].setheading(0)
+        if self.head.heading() != 180:  # Prevent moving directly backward
+            self.head.setheading(0)
 
     def move_left(self):
-        if self.snake_body[0].heading() != 0:  # Prevent moving directly backward
-            self.snake_body[0].setheading(180)
+        if self.head.heading() != 0:  # Prevent moving directly backward
+            self.head.setheading(180)
 
     def move(self):
         for snake_body_index in range(len(self.snake_body)-1, 0, -1):
@@ -38,4 +41,4 @@ class Snake:
             new_y = self.snake_body[snake_body_index-1].ycor()
             self.snake_body[snake_body_index].goto(new_x, new_y)
         
-        self.snake_body[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
