04fff5f26083d8a83d97be35aff9cd05ce0fac61
Zach Phillips <phillizr@udel.edu>
2024-08-09 12:02:17 -0400

    Initial commit for Turtle Crossing project.

---
+++ day_23_turtle_crossing/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+
+
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+
+
+screen.exitonclick()
