0973e898fe7dc65b46d779d283983b17a4d28564
Zach Phillips <phillizr@udel.edu>
2024-08-09 18:47:59 -0400

    Added persistant high score.

--- day_20_snake/main.py
+++ day_20_snake/main.py
@@ -38,14 +38,16 @@ while game_is_on:
 
     # Detect wall collision
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
+        # game_is_on = False
 
     # Detect tail collision
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            # game_is_on = False
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
--- day_20_snake/scoreboard.py
+++ day_20_snake/scoreboard.py
@@ -3,12 +3,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 12, "normal")
 
+with open("high_score.txt", 'r') as high_score_file:
+    high_score = int(high_score_file.read())  # Convert to integer
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = high_score
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -18,13 +22,15 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.clear()
-        self.home()
-        self.write(f"GAME OVER!!! Your score was {self.score}", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        with open("high_score.txt", 'w') as high_score_file:
+            high_score_file.write(str(self.high_score))  # Convert to string before writing
